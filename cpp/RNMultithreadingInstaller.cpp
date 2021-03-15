ef34c30e4b4f95f417ffbca9c733a6252a13b5cf
Marc Rousavy <marcrousavy@hotmail.com>
2021-03-15 19:59:04 +0100

    Update RNMultithreadingInstaller.cpp

--- cpp/RNMultithreadingInstaller.cpp
+++ cpp/RNMultithreadingInstaller.cpp
@@ -1,32 +1,26 @@
 #include "RNMultithreadingInstaller.h"
+#include "ThreadPool.h"
+
+#define MAX_THREAD_COUNT 2
 
 namespace mrousavy {
 namespace multithreading {
 void install(jsi::Runtime& runtime) {
+  ThreadPool pool(MAX_THREAD_COUNT);
+  
   // spawnThread(() => Promise<void>)
   auto spawnThread = jsi::Function::createFromHostFunction(runtime,
-                                                           jsi::PropNameID::forAscii(jsiRuntime, "spawnThread"),
+                                                           jsi::PropNameID::forAscii(runtime, "spawnThread"),
                                                            1,  // func
-                                                           [](jsi::Runtime& runtime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
-    
+                                                           [&pool](jsi::Runtime& runtime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
     auto function = arguments[0].asObject(runtime).asFunction(runtime);
-    std::thread
+    pool.enqueue([=]() {
+      return 0;
+    });
     
-    auto keyName = convertJSIStringToNSString(runtime, arguments[0].getString(runtime));
-    
-    if (arguments[1].isBool()) {
-      [MMKV.defaultMMKV setBool:arguments[1].getBool() forKey:keyName];
-    } else if (arguments[1].isNumber()) {
-      [MMKV.defaultMMKV setDouble:arguments[1].getNumber() forKey:keyName];
-    } else if (arguments[1].isString()) {
-      auto stringValue = convertJSIStringToNSString(runtime, arguments[1].getString(runtime));
-      [MMKV.defaultMMKV setString:stringValue forKey:keyName];
-    } else {
-      throw jsi::JSError(runtime, "MMKV::set: 'value' argument is not of type bool, number or string!");
-    }
     return jsi::Value::undefined();
   });
-  jsiRuntime.global().setProperty(jsiRuntime, "mmkvSet", std::move(mmkvSet));
+  runtime.global().setProperty(runtime, "spawnThread", std::move(spawnThread));
 }
 } // namespace multithreading
 } // namespace mrousavy
