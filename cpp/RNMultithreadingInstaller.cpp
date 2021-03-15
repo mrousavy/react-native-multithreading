effd62c8af47894265d92d2ebf5f8ecd9305a2c8
Marc Rousavy <marcrousavy@hotmail.com>
2021-03-15 20:12:21 +0100

    Impl

--- cpp/RNMultithreadingInstaller.cpp
+++ cpp/RNMultithreadingInstaller.cpp
@@ -7,6 +7,7 @@ namespace mrousavy {
 namespace multithreading {
 void install(jsi::Runtime& runtime) {
   ThreadPool pool(MAX_THREAD_COUNT);
+  // TODO: Create runtimes for each thread pool? can I do on-demand instead?
   
   // spawnThread(() => Promise<void>)
   auto spawnThread = jsi::Function::createFromHostFunction(runtime,
@@ -14,9 +15,37 @@ void install(jsi::Runtime& runtime) {
                                                            1,  // func
                                                            [&pool](jsi::Runtime& runtime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
     auto function = arguments[0].asObject(runtime).asFunction(runtime);
-    pool.enqueue([=]() {
-      return 0;
+    auto spawnThreadCallback = jsi::Function::createFromHostFunction(runtime,
+                                                                     jsi::PropNameID::forAscii(runtime, "spawnThreadCallback"),
+                                                                     2,
+                                                                     [&](jsi::Runtime& runtime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
+      auto resolver = [&runtime, &arguments](jsi::Value value) {
+        arguments[0]
+          .asObject(runtime)
+          .asFunction(runtime)
+          .call(runtime, value);
+      };
+      auto rejecter = [&runtime, &arguments](std::string message) {
+        arguments[1]
+          .asObject(runtime)
+          .asFunction(runtime)
+          .call(runtime, jsi::JSError(runtime, message));
+      };
+      // TODO: Adapt Function -> Shared Value
+      pool.enqueue([&resolver, &rejecter]() {
+        try {
+          // TODO: Call adapted function and get result back
+          auto result = jsi::Value(42);
+          resolver(result);
+        } catch (std::exception& exc) {
+          rejecter(exc.what());
+        }
+      });
+      return jsi::Value::undefined();
     });
+    
+    auto newPromise = runtime.global().getProperty(runtime, "Promise");
+    auto promise = newPromise.asObject(runtime).asFunction(runtime).call(runtime, spawnThreadCallback);
     
     return jsi::Value::undefined();
   });
