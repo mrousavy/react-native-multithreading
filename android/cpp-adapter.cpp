f7e3da678120fe21fa4bdf29e69c9c9e19cbbc7a
Marc Rousavy <marcrousavy@hotmail.com>
2021-03-16 13:26:27 +0100

    Base

--- android/cpp-adapter.cpp
+++ android/cpp-adapter.cpp
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <jsi/jsi.h>
+#include <jsi/JSIDynamic.h>
 #include "RNMultithreadingInstaller.h"
 #include "AndroidErrorHandler.h"
 #include "AndroidScheduler.h"
@@ -9,10 +10,17 @@ using namespace facebook;
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_reactnativemultithreading_MultithreadingModule_nativeInstallMultithreading(JNIEnv *env, jobject clazz, jlong jsiPtr, reanimated::AndroidScheduler scheduler) {
+Java_com_reactnativemultithreading_MultithreadingModule_nativeInstallMultithreading(JNIEnv *env,
+        jobject clazz,
+        jlong jsiPtr,
+        jni::alias_ref<facebook::react::CallInvokerHolder::javaobject> jsCallInvokerHolder,
+        jni::alias_ref<AndroidScheduler::javaobject> androidScheduler) {
     auto runtime = reinterpret_cast<jsi::Runtime*>(jsiPtr);
     return mrousavy::multithreading::install(*runtime, []() {
-        return std::make_shared<reanimated::AndroidScheduler>(nullptr);
+        auto jsCallInvoker = jsCallInvokerHolder->cthis()->getCallInvoker();
+        auto scheduler = androidScheduler->cthis()->getScheduler();
+        scheduler->setJSCallInvoker(jsCallInvoker);
+        return scheduler;
     }, [](std::shared_ptr<reanimated::AndroidScheduler> scheduler) {
         return std::make_shared<reanimated::AndroidErrorHandler>(scheduler);
     });
