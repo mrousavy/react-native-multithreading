b25f10ba145136cf1a7b69d931a6133157bacccb
Marc Rousavy <marcrousavy@hotmail.com>
2021-04-02 14:51:08 +0200

    Add support for V8 JS engine

--- cpp/MakeJSIRuntime.h
+++ cpp/MakeJSIRuntime.h
@@ -1,10 +1,16 @@
 #pragma once
 
 #include <jsi/jsi.h>
+#include <memory>
 
 #if __has_include(<hermes/hermes.h>)
+// Hermes (https://hermesengine.dev)
 #include <hermes/hermes.h>
+#elif __has_include(<v8runtime/V8RuntimeFactory.h>)
+// V8 (https://github.com/Kudo/react-native-v8)
+#include <v8runtime/V8RuntimeFactory.h>
 #else
+// JSC
 #include <jsi/JSCRuntime.h>
 #endif
 
@@ -14,11 +20,13 @@ namespace mrousavy {
 namespace multithreading {
 
 static std::unique_ptr<jsi::Runtime> makeJSIRuntime() {
-  #if __has_include(<hermes/hermes.h>)
+#if __has_include(<hermes/hermes.h>)
   return facebook::hermes::makeHermesRuntime();
-  #else
+#elif __has_include(<v8runtime/V8RuntimeFactory.h>)
+  return facebook::createV8Runtime("");
+#else
   return facebook::jsc::makeJSCRuntime();
-  #endif
+#endif
 }
   
 }
