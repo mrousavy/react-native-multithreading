1f3398a32252ad892feffb0de90f9c8f340ae58a
Marc Rousavy <marcrousavy@hotmail.com>
2021-04-12 10:54:57 +0200

    Revert "Depend on libreanimated"
    This reverts commit 78ec9e832584575f1dd14f505af9eb1be4cb677d.

--- android/src/main/JNI/Android.mk
+++ android/src/main/JNI/Android.mk
@@ -2,22 +2,30 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := rnmultithreading
+LOCAL_MODULE := reanimated
 
 PROJECT_FILES := $(wildcard $(LOCAL_PATH)/../cpp/*.cpp)
-PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../Common/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../Common/cpp/*.cpp)
+PROJECT_FILES += $(wildcard $(LOCAL_PATH)/../Common/cpp/**/*.cpp)
 
 PROJECT_FILES := $(PROJECT_FILES:$(LOCAL_PATH)/%=%)
 
 LOCAL_SRC_FILES := $(PROJECT_FILES)
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../Common
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../Common/cpp/hidden_headers
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../cpp/headers
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../Common/cpp/headers
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../Common/cpp/headers/NativeModules
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../Common/cpp/headers/Registries
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../Common/cpp/headers/SharedItems
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../Common/cpp/headers/SpecTools
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../Common/cpp/headers/Tools
 
 LOCAL_CFLAGS += -DONANDROID -fexceptions -frtti
 
 LOCAL_STATIC_LIBRARIES := libjsi callinvokerholder
-LOCAL_SHARED_LIBRARIES := libfolly_json libfbjni libreactnativejni libreanimated
+LOCAL_SHARED_LIBRARIES := libfolly_json libfbjni libreactnativejni
 
 include $(BUILD_SHARED_LIBRARY)
 
--- android/src/main/JNI/react/Android.mk
+++ android/src/main/JNI/react/Android.mk
@@ -23,7 +23,7 @@ LOCAL_CFLAGS += -fexceptions -frtti -Wno-unused-lambda-capture
 LOCAL_LDLIBS += -landroid
 
 # The dynamic libraries (.so files) that this module depends on.
-LOCAL_SHARED_LIBRARIES := libfolly_json libfb libfbjni libglog_init libyoga libreanimated
+LOCAL_SHARED_LIBRARIES := libfolly_json libfb libfbjni libglog_init libyoga
 
 # The static libraries (.a files) that this module depends on.
 LOCAL_STATIC_LIBRARIES := libreactnative libcallinvokerholder
