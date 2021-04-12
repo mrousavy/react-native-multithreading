9d30527c1d779e86ae9af74f7931d4e33c14989c
Marc Rousavy <marcrousavy@hotmail.com>
2021-04-12 10:55:16 +0200

    Revert "Copy over .mk files from REA"
    This reverts commit c15bac59a02a610248d9cf7cdbae7e79f92ecd66.

---
+++ android/cpp-adapter.cpp
@@ -0,0 +1,40 @@
+#include <jni.h>
+#include <jsi/jsi.h>
+#include <memory>
+
+#include "RNMultithreadingInstaller.h"
+
+#include "Tools/Scheduler.h"
+#include "SpecTools/ErrorHandler.h"
+
+using namespace facebook;
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_reactnativemultithreading_MultithreadingModule_nativeInstallMultithreading(JNIEnv *env, jobject clazz, jlong jsiPtr) {
+    auto runtime = reinterpret_cast<jsi::Runtime*>(jsiPtr);
+
+    auto makeScheduler = []() -> std::shared_ptr<reanimated::Scheduler> {
+        return std::shared_ptr<reanimated::Scheduler>();
+    };
+    auto makeErrorHandler = []() -> std::shared_ptr<reanimated::ErrorHandler> {
+        return std::shared_ptr<reanimated::ErrorHandler>();
+
+    };
+    mrousavy::multithreading::install(*runtime, makeScheduler, makeErrorHandler);
+/*
+
+    // TODO: Implement multithreading for Android.
+    //  The only problem I have with this is that I cannot really import/include the Reanimated library since that is a prebuilt .aar.
+    //  That means, I cannot import it's headers, I cannot link it, and I cannot create instances of ShareableValue, AndroidErrorHandler, AndroidScheduler, ...
+    runtime->global().setProperty(*runtime,
+            "spawnThread",
+            jsi::Function::createFromHostFunction(*runtime,
+                    jsi::PropNameID::forAscii(*runtime, "spawnThread"),
+                    1,
+                    [](jsi::Runtime& runtime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
+                auto promise = runtime.global().getProperty(runtime, "Promise").asObject(runtime);
+                auto rejecter = promise.getProperty(runtime, "reject");
+                return rejecter.asObject(runtime).asFunction(runtime).call(runtime, jsi::JSError(runtime, "Multithreading is not yet supported on Android.").value());
+    }));*/
+}
