9837ff898f1824ea801479cf1f75b1f6fadceca1
Marc Rousavy <marcrousavy@hotmail.com>
2021-04-13 19:50:42 +0200

    Fix ::install signature

--- cpp/RNMultithreadingInstaller.h
+++ cpp/RNMultithreadingInstaller.h
@@ -17,8 +17,8 @@ namespace mrousavy {
 namespace multithreading {
 
 void install(jsi::Runtime& runtime,
-             std::function<std::shared_ptr<reanimated::Scheduler>()> makeScheduler,
-             std::function<std::shared_ptr<reanimated::ErrorHandler>(std::shared_ptr<reanimated::Scheduler>)> makeErrorHandler);
+             const std::function<std::shared_ptr<reanimated::Scheduler>()>& makeScheduler,
+             const std::function<std::shared_ptr<reanimated::ErrorHandler>(std::shared_ptr<reanimated::Scheduler>)>& makeErrorHandler);
 
 }
 }
