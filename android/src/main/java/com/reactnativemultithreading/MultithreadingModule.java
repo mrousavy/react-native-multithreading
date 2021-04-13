de1840bafa49929da4e1e6f9182a9fe7644b1bb5
Marc Rousavy <marcrousavy@hotmail.com>
2021-04-13 21:07:43 +0200

    Update MultithreadingModule.java

--- android/src/main/java/com/reactnativemultithreading/MultithreadingModule.java
+++ android/src/main/java/com/reactnativemultithreading/MultithreadingModule.java
@@ -2,6 +2,7 @@ package com.reactnativemultithreading;
 
 import androidx.annotation.NonNull;
 
+import com.facebook.react.bridge.JavaScriptContextHolder;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 
@@ -12,8 +13,8 @@ public class MultithreadingModule extends ReactContextBaseJavaModule {
 
   private static native void nativeInstallMultithreading(long jsiPtr);
 
-    nativeInstallMultithreading(jsiRuntimePointer);
   public static void install(ReactApplicationContext context, JavaScriptContextHolder jsContext) {
+    nativeInstallMultithreading(jsContext.get());
   }
 
   // Dummy so react native adds it to the Gradle Module System
