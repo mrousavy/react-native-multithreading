bf1f1b8aaa06c977c614dfc1ac4fbf211e996782
Marc Rousavy <marcrousavy@hotmail.com>
2021-04-13 20:22:47 +0200

    Use provided `MultithreadingJSIModulePackage`

--- android/src/main/java/com/reactnativemultithreading/MultithreadingJSIModulePackage.java
+++ android/src/main/java/com/reactnativemultithreading/MultithreadingJSIModulePackage.java
@@ -1,17 +1,19 @@
 package com.reactnativemultithreading;
 
-import com.facebook.react.bridge.JSIModulePackage;
 import com.facebook.react.bridge.JSIModuleSpec;
 import com.facebook.react.bridge.JavaScriptContextHolder;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
-import java.util.Collections;
 import java.util.List;
 
-public class MultithreadingJSIModulePackage implements JSIModulePackage {
+public class MultithreadingJSIModulePackage extends ReanimatedJSIModulePackage {
     @Override
     public List<JSIModuleSpec> getJSIModules(ReactApplicationContext reactApplicationContext, JavaScriptContextHolder jsContext) {
-        MultithreadingModule.install(jsContext.get());
-        return Collections.emptyList();
+        try {
+            return super.getJSIModules(reactApplicationContext, jsContext);
+        } finally {
+            MultithreadingModule.install(jsContext.get());
+        }
     }
 }
--- example/android/app/src/main/java/com/example/reactnativemultithreading/MainApplication.java
+++ example/android/app/src/main/java/com/example/reactnativemultithreading/MainApplication.java
@@ -10,10 +10,11 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
+import com.reactnativemultithreading.MultithreadingJSIModulePackage;
 import com.reactnativemultithreading.MultithreadingPackage;
 
 import com.facebook.react.bridge.JSIModulePackage;
-import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -41,7 +42,7 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected JSIModulePackage getJSIModulePackage() {
-          return new ExampleJSIModulePackage();
+          return new MultithreadingJSIModulePackage();
         }
       };
 
