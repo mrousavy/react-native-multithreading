7e0c81e9186981b24906fdd03a6de1e22dd5904c
Marc Rousavy <marcrousavy@hotmail.com>
2021-04-18 17:17:49 +0200

    Add android build script

---
+++ scripts/build-android.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CWD="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+cd $CWD
+cd ..
+
+echo 'Building Android in:'
+echo $PWD
+
+cd android
+rm -rf build/
+
+./gradlew clean
+./gradlew assembleDebug assembleRelease
+
+rm -rf ../android-npm
+mkdir ../android-npm
+
+shopt -s dotglob nullglob
+mv -v build/outputs/aar/* ../android-npm
